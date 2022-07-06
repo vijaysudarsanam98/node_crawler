34da60ac5588a77781717be5c5b77081cdc9b2a1
SudarsanRamachandran <97380122+SudarsanRamachandran@users.noreply.github.com>
2022-07-06 17:42:15 +0530

    crawler_done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const Crawler = require("crawler");
 
-app.get('/', (req, res) => {
-  res.send('crawler started');
-});
+let obselete = []; // Array of what was crawled already
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+let c = new Crawler();
+
+function crawlAllUrls(url) {
+    console.log(`Crawling ${url}`);
+    c.queue({
+        uri: url,
+        callback: function (err, res, done) {
+            if (err) throw err;
+            let $ = res.$;
+            try {
+                let urls = $("a");
+                Object.keys(urls).forEach((item) => {
+                    if (urls[item].type === 'tag') {
+                        let href = urls[item].attribs.href;
+                        if (href && !obselete.includes(href) && href.startsWith(url)) {
+                            href = href.trim();
+                            obselete.push(href);
+                            // Slow down the
+                            setTimeout(function() {
+                                href.startsWith('http') ? crawlAllUrls(href) : crawlAllUrls(`${url}${href}`) // The latter might need extra code to test if its the same site and it is a full domain with no URI
+                            }, 5000)
+                        }
+                    }
+                });
+            } catch (e) {
+                console.error(`Encountered an error crawling ${url}. Aborting crawl.`);
+                done()
+            }
+            done();
+        }
+    })
+}
+
+crawlAllUrls(url);
